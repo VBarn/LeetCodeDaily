48195654a49e78266ce96f43608a8100d6bf1800
VBarn <vipul.barnwal@gmail.com>
2023-11-29 13:38:10 +0530

    LC 191

--- dailyChallenge/src/main/java/org/example/Main.java
+++ dailyChallenge/src/main/java/org/example/Main.java
@@ -95,4 +95,13 @@ public class Main {
             return (int)sol;
         return 0;
     }
+
+    //191. Hamming weight
+    public int hammingWeight(int n) {
+        int sol=0;
+        for(int i=0;i<32;i++)
+            if(((1<<i)&n)!=0)
+                sol++;
+        return sol;
+    }
 }
