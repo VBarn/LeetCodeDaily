95a5d9ecffd998af7cf80e983e0fc7854d288ead
VBarn <vipul.barnwal@gmail.com>
2023-11-29 14:38:56 +0530

    LC 76

--- dailyChallenge/src/main/java/org/example/HardPractice.java
+++ dailyChallenge/src/main/java/org/example/HardPractice.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class HardPractice {
     public static void main(String[] args){
         HardPractice hp=new HardPractice();
-        hp.minWindow("ADOBECODEBANC","ABC");
+        int[] test={1,2,3};
+        hp.findPeakElement(test);
     }
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> sol=new ArrayList<>();
@@ -74,7 +75,7 @@ public class HardPractice {
         }
     }
 
-    //162. Find Peak Element
+    //76. Minimum Window Substring
     public String minWindow(String s, String t) {
         int[] mem=new int[1+'z'-'A'];
         for(char c:t.toCharArray())
@@ -106,5 +107,28 @@ public class HardPractice {
             if(cur[i]<mem[i])
                 return false;
         return true;
+    }
+
+    //162. Find Peak Element
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=low+((high-low)/2);
+            if(mid==0)
+                return nums[0]<nums[1]?1:0;
+            if(mid==n-1)
+                return nums[n-1]<nums[n-2]?n-2:n-1;
+            if(nums[mid]<nums[mid+1])
+                low=mid+1;
+            else{
+                if(nums[mid-1]<nums[mid])
+                    return mid;
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+        return 0;
     }
 }
